6d2398f00d4fe7546fb2d47cd5e9f2df2daede8f
Aleksandra Kashina <Borland3711@gmail.com>
2024-06-03 18:23:38 -0400

    Update test

--- 4 chapter/smoke_test.py
+++ 4 chapter/smoke_test.py
@@ -1,7 +1,5 @@
-import time
 
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
@@ -36,13 +34,27 @@ product_1 = driver.find_element(By.XPATH, '//a[@id="item_4_title_link"]')
 value_product_1 = product_1.text
 print(value_product_1)
 
-price_product_1 = driver.find_element(By.XPATH, '//div[@class="inventory_item_price"]')
+price_product_1 = driver.find_element(By.XPATH, '(//div[@class="inventory_item_price"])[1]')
 value_price_product_1 = price_product_1.text
 print(value_price_product_1)
 
+'''Info product 2'''
+product_2 = driver.find_element(By.XPATH, '//a[@id="item_1_title_link"]')
+value_product_2 = product_2.text
+print(value_product_2)
+
+price_product_2 = driver.find_element(By.XPATH, '(//div[@class="inventory_item_price"])[3]')
+value_price_product_2 = price_product_2.text
+print(value_price_product_2)
+
+'''Adding products to cart'''
 add_to_cart_product_1 = driver.find_element(By.XPATH, '//button[@id="add-to-cart-sauce-labs-backpack"]')
 add_to_cart_product_1.click()
 print('Add the Product 1 to the cart')
+
+add_to_cart_product_2 = driver.find_element(By.XPATH, '//button[@id="add-to-cart-sauce-labs-bolt-t-shirt"]')
+add_to_cart_product_2.click()
+print('Add the Product 2 to the cart')
 
 cart = driver.find_element(By.XPATH, '//div[@id="shopping_cart_container"]')
 cart.click()
@@ -53,14 +65,28 @@ cart_product_1 = driver.find_element(By.XPATH, '//a[@id="item_4_title_link"]')
 cart_value_product_1 = cart_product_1.text
 print(cart_value_product_1)
 assert value_product_1 == cart_value_product_1
-print('Cart name info is correct')
+print('Cart name info for product 1 is correct')
 
-cart_price_product_1 = driver.find_element(By.XPATH, '//div[@class="inventory_item_price"]')
+cart_price_product_1 = driver.find_element(By.XPATH, '(//div[@class="inventory_item_price"])[1]')
 cart_value_price_product_1 = cart_price_product_1.text
 print(cart_value_price_product_1)
 assert value_price_product_1 == cart_value_price_product_1
-print('Cart price info is correct')
+print('Cart price info for product 1 is correct')
 
+'''Info Cart Product 2'''
+cart_product_2 = driver.find_element(By.XPATH, '//a[@id="item_1_title_link"]')
+cart_value_product_2 = cart_product_2.text
+print(cart_value_product_2)
+assert value_product_2 == cart_value_product_2
+print('Cart name info for product 2 is correct')
+
+cart_price_product_2 = driver.find_element(By.XPATH, '(//div[@class="inventory_item_price"])[2]')
+cart_value_price_product_2 = cart_price_product_2.text
+print(cart_value_price_product_2)
+assert value_price_product_2 == cart_value_price_product_2
+print('Cart price info for product 2 is correct')
+
+'''Checkout'''
 checkout = driver.find_element(By.XPATH, '//button[@id="checkout"]')
 checkout.click()
 print('Click Checkout button')
@@ -92,23 +118,43 @@ print(delivery_value_product_1)
 assert value_product_1 == delivery_value_product_1
 print('Delivery name info is correct')
 
-delivery_price_product_1 = driver.find_element(By.XPATH, '//div[@class="inventory_item_price"]')
+delivery_price_product_1 = driver.find_element(By.XPATH, '(//div[@class="inventory_item_price"])[1]')
 delivery_value_price_product_1 = delivery_price_product_1.text
 print(delivery_value_price_product_1)
 assert value_price_product_1 == delivery_value_price_product_1
 print('Delivery price info is correct')
 
+'''Info Delivery Product 2'''
+delivery_product_2 = driver.find_element(By.XPATH, '//a[@id="item_1_title_link"]')
+delivery_value_product_2 = delivery_product_2.text
+print(delivery_value_product_2)
+assert value_product_2 == delivery_value_product_2
+print('Delivery name info for product 2 is correct')
+
+delivery_price_product_2 = driver.find_element(By.XPATH, '(//div[@class="inventory_item_price"])[2]')
+delivery_value_price_product_2 = delivery_price_product_2.text
+print(delivery_value_price_product_2)
+assert value_price_product_2 == delivery_value_price_product_2
+print('Delivery price info for product 2 is correct')
+
+
+price_1 = float(delivery_value_price_product_1.replace('$', ''))
+price_2 = float(delivery_value_price_product_2.replace('$', ''))
+sum_products = price_1 + price_2
+
 total_price = driver.find_element(By.XPATH, '//div[@data-test="subtotal-label"]')
 value_total_price = total_price.text
 print(value_total_price)
-total_item = 'Item total: ' + delivery_value_price_product_1
-print(str(total_item))
-assert value_total_price == total_item
+value_total_price_num = float(value_total_price.replace('Item total: $', ''))
+print(sum_products)
+assert value_total_price_num == sum_products
 print('The price is same')
+
 
 finish_button = driver.find_element(By.XPATH, '//button[@id="finish"]')
 finish_button.click()
 
+'''Gratitude page'''
 gratitude_for_order = driver.find_element(By.XPATH, '//h2[@class="complete-header"]').text
 assert gratitude_for_order == 'Thank you for your order!'
 print('The gratitude is correct')
